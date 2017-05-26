d91d7914ba200d24f09bac9a09932ea4c3b42661
Fly <393830887@qq.com>
2017-05-26 20:41:19 +0800

    Add 简单的 MonoAdaptorInspector

---
+++ ILRuntimeDemo/Assets/Editor/ILRuntime/MonoAdaptorInspector.cs
@@ -0,0 +1,30 @@
+﻿
+using ILRuntime.Runtime.Adaptor;
+using UnityEditor;
+
+[CustomEditor(typeof(MonoBehaviourAdapter.MonoAdaptor), true)]
+public class MonoAdaptorInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var adaptor = target as MonoBehaviourAdapter.MonoAdaptor;
+        var instance = adaptor.ILInstance;
+
+        if (instance != null)
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("ILType:", instance.Type.Name);
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("MonoMessage:");
+            EditorGUI.indentLevel = 1;
+            foreach (var pair in adaptor.MonoMethodDict)
+            {
+                EditorGUILayout.LabelField(pair.Key);
+            }
+            EditorGUI.indentLevel = 0;
+        }
+    }
+}
